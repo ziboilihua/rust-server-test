dfdc9b50c3554c7ed9fd8b36f861d2d43102e6bf
zhouzibo <18208965365@163.com>
2021-03-23 20:02:20 +0800

    add comments

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ use std::fs;
 fn main() {
     // 在本地7878端口创建TCP连接
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // 读取异常响应文件html
     let res_html = fs::read_to_string("error.html").unwrap();
+    // 生成异常响应信息字符串
     let err_resp = format!(
         "HTTP/1.1 404 NOTFOUND\r\nContent-Length: {}\r\n\r\n{}",
         res_html.len(),
         res_html
     );
+    // 读取正常响应文件html res_html 变量隐藏
     let res_html = fs::read_to_string("hello.html").unwrap();
+    // 生成正常返回信息字符串
     let success_resp = format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         res_html.len(),
@@ -19,9 +23,13 @@ fn main() {
     );
     // 监听TCP连接
     for stream in listener.incoming() {
+        // 获取tcpStream (如果失败直接panic)
         let mut stream = stream.unwrap();
+        // 打印请求信息
         let req = print_request(&stream);
+        // 通过请求信息判断http请求
         let result = handle_http_method(req.as_str());
+        // 生成响应信息
         let resp = match result {
             Ok(m) => {
                 print!("request method: {}", m.as_str());
@@ -29,7 +37,9 @@ fn main() {
             },
             Err(_) => err_resp.as_str()
         };
+        // 将响应信息写入到流中
         stream.write(resp.as_bytes());
+        // 刷新流
         stream.flush().unwrap();
     }
 }
@@ -43,12 +53,13 @@ fn print_request(mut stream: &TcpStream) -> String {
     let content = String::from_utf8_lossy(&buffer);
     // 打印请求内容
     print!("{}", content);
+    // 返回请求信息
     content.to_string()
 }
 
 
 fn handle_http_method (content: &str) -> Result<HttpMethod, &str> {
-    // 根据请求内容判断HTTP请求method 正确则返回包含HTTPMethod的OK枚举,非HTTPMethod则返回包含错误信息ERR枚举
+    // 根据请求内容判断HTTP请求method 正确则返回包含HTTPMethod的OK枚举,非GET POST则返回包含错误信息ERR枚举
     if content.starts_with("GET") {
         Result::Ok(HttpMethod::GET)
     } else if content.starts_with("POST") {
@@ -64,6 +75,7 @@ enum HttpMethod {
 }
 
 impl HttpMethod {
+    // 实现as_str方法便于打印
     pub(crate) fn as_str(&self) -> &'static str {
         match *self {
             HttpMethod::GET => "GET",
